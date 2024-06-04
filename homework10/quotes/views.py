d99c3dd9884a548a31b35535f0feae7956ec694d
MarynaProkopchuk <9964649@gmail.com>
2024-06-04 23:23:19 +0300

    commit2

--- homework10/quotes/views.py
+++ homework10/quotes/views.py
@@ -3,8 +3,10 @@ from .utils import get_database
 from django.core.paginator import Paginator
 
 
-def main(request):
+def main(request, page=1):
     db = get_database()
     quotes = db.quotes.find()
-    # return print(quotes)
-    return render(request, "quotes/index.html", context={"quotes": quotes})
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page)
+    quotes_on_page = paginator.page(page)
+    return render(request, "quotes/index.html", context={"quotes": quotes_on_page})
